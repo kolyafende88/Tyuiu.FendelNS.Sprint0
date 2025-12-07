96eef9b2cb78447d17d9d3e8c989824c459f5412
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 18:53:28 +0500

    Task2

---
+++ Tyuiu.FendelNS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.FendelNS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.FendelNS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            var name = "Коля";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Коля", res);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.FendelNS.Sprint0.Task2.V0.Lib;  
+namespace Tyuiu.FendelNS.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Коля"));
+            Console.ReadKey();
+        }
+    }
+}
