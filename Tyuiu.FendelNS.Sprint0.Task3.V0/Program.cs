b8a80c73327963b8db92e2523a7afced0c2ea4a2
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 20:20:11 +0500

    Task3

---
+++ Tyuiu.FendelNS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.FendelNS.Sprint0.Task3.V0.Lib;  
+namespace Tyuiu.FendelNS.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7,7));
+            Console.ReadKey();
+        }
+    }
+}
