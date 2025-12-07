cf641c12df12164c3a6f1c0c733491021d710ca8
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 18:13:35 +0500

    Task1

--- Tyuiu.FendelNS.Task0.V0/Program.cs
+++ Tyuiu.FendelNS.Task0.V0/Program.cs
@@ -1,10 +1,4 @@
 ﻿namespace Tyuiu.FendelNS.Task0.V0
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+  
 }
